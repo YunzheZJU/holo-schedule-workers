39369db7a40106ef75a245044d614ced21a9f130
Yunzhe <yunzhe@zju.edu.cn>
2020-06-04 00:01:28 +0800

    Add guards for publishAll.

--- scripts/publish-all.js
+++ scripts/publish-all.js
@@ -3,22 +3,27 @@ const { configure, publish, switchPackage } = require('./shared')
 const publishConfig = require('../publish.json')
 
 const publishAll = async () => {
-  const packages = process.argv[2]
-    ? [process.argv[2]]
-    : Object.keys(publishConfig)
+  const packageName = process.argv[2]
+
+  if (!packageName) {
+    console.error('Please specify a package name')
+    return
+  }
+
+  if (!(packageName in publishConfig)) {
+    console.log(`Skipping ${packageName} which is absent in publish.json...`)
+    return
+  }
+
+  switchPackage(packageName)
 
   // eslint-disable-next-line no-restricted-syntax
-  for (const packageName of packages) {
-    switchPackage(packageName)
-
-    // eslint-disable-next-line no-restricted-syntax
-    for (const worker of publishConfig[packageName]) {
-      console.log(`Publishing ${packageName} to ${worker}...`)
-      /* eslint-disable no-await-in-loop */
-      await configure(worker)
-      await publish()
-      /* eslint-enable no-await-in-loop */
-    }
+  for (const worker of publishConfig[packageName]) {
+    console.log(`Publishing ${packageName} to ${worker}...`)
+    /* eslint-disable no-await-in-loop */
+    await configure(worker)
+    await publish()
+    /* eslint-enable no-await-in-loop */
   }
 }
 
